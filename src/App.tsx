6e57e15541f3e429ef8c62c5adab55e25c6301f9
Behniya <b3hniya@gmail.com>
2023-12-02 09:56:46 -0800

    Fix: fix the base boilerplate.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Greeting } from "./presentation/greetingModule/pages/Greeting";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Greeting />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Greeting />} />
+    </Routes>
   );
 }
 
