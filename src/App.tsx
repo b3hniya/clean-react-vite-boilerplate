0e16e1314d60596408414e5912b290933d4526b2
Behniya <b3hniya@gmail.com>
2023-12-02 10:28:34 -0800

    Feature: add store.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Greeting } from "./presentation/greetingModule/pages/Greeting";
+import { AnotherPage } from "./presentation/anotherModule/pages/AnotherPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Greeting />} />
+      <Route path="/another-page" element={<AnotherPage />} />
     </Routes>
   );
 }
---
+++ src/presentation/anotherModule/pages/AnotherPage.tsx
@@ -0,0 +1,13 @@
+import useStore from "../../../store/store";
+
+export function AnotherPage() {
+  const { count, increaseCount, decreaseCount } = useStore();
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  );
+}
