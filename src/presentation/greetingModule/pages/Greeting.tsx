5e757eb52b77e0a49d0a1e5edf1c5f714e171f4a
Behniya <b3hniya@gmail.com>
2023-12-03 04:29:18 -0800

    Feature: add tailwindcss

--- src/presentation/greetingModule/pages/Greeting.tsx
+++ src/presentation/greetingModule/pages/Greeting.tsx
@@ -1,3 +1,3 @@
 export function Greeting() {
-  return <h1> Hello World </h1>
+  return <h1 className="text-3xl font-bold underline"> Hello World </h1>
 }
--- src/presentation/translationModule/pages/TranslationTest.tsx
+++ src/presentation/translationModule/pages/TranslationTest.tsx
@@ -4,11 +4,31 @@ export function TranslationTest() {
   const { changeLanguage, display } = useInternationalization()
 
   return (
-    <>
-      <h1>{display('welcomeMessage')}</h1>
-      <button onClick={() => changeLanguage('zh')}>中文</button>
-      <button onClick={() => changeLanguage('de')}>DE</button>
-      <button onClick={() => changeLanguage('en')}>EN</button>
-    </>
+    <div className="p-4  w-[320px]">
+      <h1 className="text-center font-bold text-xl">
+        {display('welcomeMessage')}
+      </h1>
+
+      <div className="flex items-center justify-between w-full mt-8">
+        <button
+          onClick={() => changeLanguage('zh')}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          中文
+        </button>
+        <button
+          onClick={() => changeLanguage('de')}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          DE
+        </button>
+        <button
+          onClick={() => changeLanguage('en')}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          EN
+        </button>
+      </div>
+    </div>
   )
 }
